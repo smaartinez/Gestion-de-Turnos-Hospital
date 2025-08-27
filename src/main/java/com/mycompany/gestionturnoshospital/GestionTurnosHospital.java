d59fd3a603771834205322d331cd2326f2d4bc9a
Simon Martinez <>
2025-08-26 21:12:24 -0400

    Primer Commit de prueba

---
+++ src/main/java/com/mycompany/gestionturnoshospital/GestionTurnosHospital.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.gestionturnoshospital;
+
+/**
+ *
+ * @author simon
+ */
+public class GestionTurnosHospital {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
